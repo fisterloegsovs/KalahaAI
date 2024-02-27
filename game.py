ba30902fa1c254363c85263a77b2680295de726b
Anders <anders.persson@live.dk>
2024-02-27 14:16:01 +0100

    updated

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import os
 import time
 
+board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+current_player = 0
+
 def print_board(board):
     print("          Player 2")
     print(" ")
@@ -14,10 +17,21 @@ def print_board(board):
 def is_valid_move(board, player, pit):
     return board[pit] != 0 and (player == 0 and 0 <= pit <= 5) or (player == 1 and 7 <= pit <= 12)
 
+def is_game_over(board):
+    return sum(board[0:6]) == 0 or sum(board[7:13]) == 0
+
+def get_winner(board):
+    if board[6] > board[13]:
+        return 1
+    elif board[6] < board[13]:
+        return 2
+    else:
+        return 0
+
 def make_move(board, player, pit):
     stones = board[pit]
     board[pit] = 0
-    last_pit = pit  # Set last_pit to initial value of pit
+    last_pit = pit
     while stones > 0:
         pit = (pit + 1) % 14
         if pit == 6 and player == 1 or pit == 13 and player == 0:
@@ -34,20 +48,6 @@ def capture_opposite(board, player, pit):
             board[6 if player == 0 else 13] += board[opposite_pit] + 1
             board[opposite_pit] = board[pit] = 0
 
-def is_game_over(board):
-    return sum(board[0:6]) == 0 or sum(board[7:13]) == 0
-
-def get_winner(board):
-    if board[6] > board[13]:
-        return 1
-    elif board[6] < board[13]:
-        return 2
-    else:
-        return 0
-
-# Initial game state
-board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
-current_player = 0
 
 while not is_game_over(board):
     print_board(board)
@@ -56,6 +56,8 @@ while not is_game_over(board):
         try:
             pit_input = input(f"Player {current_player + 1}, choose your pit: ")
             pit = int(pit_input) - 1
+            if current_player == 1:
+                pit += 7  # Map Player 2's input to their side of the board
             if not is_valid_move(board, current_player, pit):
                 print("Invalid move. Please choose a valid pit on your side.")
             else:
